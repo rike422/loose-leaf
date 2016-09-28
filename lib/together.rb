5a266bd7935e435d5eae47b3fa3638c89f29ff75
Akira Takahashi <rike422@gmail.com>
2016-09-28 21:06:10 +0900

    rails plugin new together

---
+++ lib/together.rb
@@ -0,0 +1,3 @@
+module Together
+  # Your code goes here...
+end
---
+++ test/together_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Together::Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Together
+  end
+end
